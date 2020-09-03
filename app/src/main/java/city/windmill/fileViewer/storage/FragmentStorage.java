d96904ad2e6a98a6948a2c3db784a8aad9fb4e3e
Windmill-City <1449182174@qq.com>
2020-09-03 22:06:51 +0800

    Finish storage display

--- app/src/main/java/city/windmill/fileViewer/storage/FragmentStorage.java
+++ app/src/main/java/city/windmill/fileViewer/storage/FragmentStorage.java
@@ -4,18 +4,72 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.blankj.utilcode.util.Utils;
 import com.windmill.FileViewer.R;
 
+import java.util.List;
+
 public class FragmentStorage extends Fragment {
+    private RecyclerView viewStorageItems;
+    private List<IStorage> storages;
+
+    public FragmentStorage(List<IStorage> storages) {
+        this.storages = storages;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_storage, container, false);
+        viewStorageItems = view.findViewById(R.id.View_StorageItems);
+        viewStorageItems.setLayoutManager(new LinearLayoutManager(Utils.getApp().getApplicationContext()));
+        viewStorageItems.setAdapter(new StorageItemAdapter());
         return view;
+    }
+
+    class StorageItemAdapter extends RecyclerView.Adapter<StorageItemAdapter.ViewHolder> {
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_storageitem, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            IStorage storage = storages.get(position);
+            holder.StorageName.setText(storage.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return storages.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView StateText;
+            ImageView State;
+            TextView StorageName;
+            Button BtnWOL;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                StorageName = itemView.findViewById(R.id.StorageName);
+                BtnWOL = itemView.findViewById(R.id.BtnWOL);
+                State = itemView.findViewById(R.id.State);
+                StateText = itemView.findViewById(R.id.StateText);
+            }
+        }
     }
 }
--- app/src/main/java/city/windmill/fileViewer/storage/FragmentStorageMgr.java
+++ app/src/main/java/city/windmill/fileViewer/storage/FragmentStorageMgr.java
@@ -11,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.blankj.utilcode.util.LogUtils;
@@ -20,6 +22,8 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import com.windmill.FileViewer.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentStorageMgr extends Fragment {
     public StorageMgr storageMgr;
@@ -36,7 +40,7 @@ public class FragmentStorageMgr extends Fragment {
         loadStorageData();
         View view = inflater.inflate(R.layout.fragment_storagemgr, container, false);
         ViewPager2 pager2 = view.findViewById(R.id.ViewPage_Storage);
-        StoragePageAdapter adapter = new StoragePageAdapter(activity);
+        StoragePageAdapter adapter = new StoragePageAdapter(activity, storageMgr);
         pager2.setAdapter(adapter);
         //Bind TabLayout and ViewPager2
         TabLayout tabLayout = view.findViewById(R.id.SideNav);
@@ -65,6 +69,42 @@ public class FragmentStorageMgr extends Fragment {
             storageMgr.LoadStorage();
         } catch (IOException e) {
             LogUtils.e(e);
+        }
+    }
+
+    static class StoragePageAdapter extends FragmentStateAdapter {
+        private FragmentStorage fRemote;
+        private FragmentStorage fLocal;
+
+        public StoragePageAdapter(@NonNull FragmentActivity fragmentActivity, StorageMgr storageMgr) {
+            super(fragmentActivity);
+            List<IStorage> remote_storages = new ArrayList<>();
+            List<IStorage> local_storages = new ArrayList<>();
+            for (IStorage storage : storageMgr.storages) {
+                if (storage instanceof RemoteStorage)
+                    remote_storages.add(storage);
+                else
+                    local_storages.add(storage);
+            }
+            fRemote = new FragmentStorage(remote_storages);
+            fLocal = new FragmentStorage(local_storages);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return fRemote;
+                case 1:
+                    return fLocal;
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
         }
     }
 }
